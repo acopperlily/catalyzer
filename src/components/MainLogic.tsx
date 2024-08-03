39391f52e0cdfe90fec76f7480a141ff4705d646
acopperlily <april.copley@protonmail.com>
2024-08-03 00:34:58 -0400

    Clean up functions, add transforms

--- src/components/MainLogic.tsx
+++ src/components/MainLogic.tsx
@@ -11,29 +11,14 @@ import intros from '../data/intros';
 import outros from '../data/outros';
 import yatesShuffle from '../utils/yatesShuffle';
 
-const len: { [key: string]: number } = {
-  intros: intros.length,
-  outros: outros.length,
-  neutralNames: neutralNames.length,
-  femaleNames: femaleNames.length,
-  maleNames: maleNames.length,
-  neutralTitles: neutralTitles.length,
-  femaleTitles: femaleTitles.length,
-  maleTitles: maleTitles.length,
-  likesArr: likesArr.length,
-  dislikesArr: dislikesArr.length,
-  likeVerbs: likeVerbs.length,
-  dislikeVerbs: dislikeVerbs.length,
-  surnames: surnames.length,
-  neutralSuffixes: neutralSuffixes.length,
-  maleSuffixes: maleSuffixes.length
-};
-
+// Convert numerals to words
 const nums: string[] = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six'];
 
+// Constants to balance number of likes & dislikes
 const VALUE1: number = 2;
 const VALUE2: number = 3;
 
+// Shuffle the arrays on every page load because I feel like it
 for (let arr of [neutralNames, femaleNames, maleNames, neutralTitles, femaleTitles, maleTitles, surnames, neutralSuffixes, maleSuffixes, intros, outros, likesArr, dislikesArr, likeVerbs, dislikeVerbs]) {
   yatesShuffle(arr);
 }
@@ -63,136 +48,173 @@ const MainLogic = () => {
 
   // Connect array elements into a coherent string
   const formatActivities = (arr: string[], conj: string): string => {
+
+    // This shouldn't happen, but just in case...
+    if (arr.length < 2) return arr[0] || 'stuff';
+
+    // Add 'and' or 'or' only
     if (arr.length === 2) return `${arr[0]} ${conj} ${arr[1]}`;
 
     // Oxford comma, bitch
     return `${arr.slice(0, -1).join(', ')}, ${conj} ${arr.slice(-1)}`;
   }
 
-  const formattedLikes: string = formatActivities(likes, 'and');
-  const formattedDislikes: string = formatActivities(dislikes, 'or');
-
-  // Use this for everything
+  // Use this for all of the things
   const getRandomNumber = (n: number): number => {
     return Math.floor(Math.random() * n);
   };
 
+  // Random & arbitrary af, but I do what I want
   const getGender = (): string => {
+
     const gender: number = getRandomNumber(10);
     if (gender > 6) return 'f';
     if (gender < 3) return 'm';
+
     return 'n';
   };
 
   const getTitle = (gender: string): string => {
-    const neutralTitle: string = neutralTitles[getRandomNumber(len.neutralTitles)];
-    const femaleTitle: string = femaleTitles[getRandomNumber(len.femaleTitles)];
-    const maleTitle: string = maleTitles[getRandomNumber(len.maleTitles)];
+
+    // Just gonna grab all possibilities so we're not duplicating code
+    const [neutralTitle, femaleTitle, maleTitle]: string[] = [neutralTitles, femaleTitles, maleTitles].map(titles => titles[getRandomNumber(titles.length)]);
+
     let roll: number = getRandomNumber(2);
     if (gender === 'n') {
+
+      // Neutral can have any title
       roll = getRandomNumber(3);
-      if (roll === 0) return neutralTitle;
-      if (roll === 1) return femaleTitle;
-      return maleTitle;
+
+      // Ooh, let's "switch" it up lol
+      switch (roll) {
+        case 0: return neutralTitle;
+        case 1: return femaleTitle;
+        default: return maleTitle;
+      }
     }
+
     if (gender === 'f') {
       if (roll === 0) return neutralTitle;
       return femaleTitle;
     }
+
+    // gender === 'm'
     if (roll === 0) return neutralTitle;
     return maleTitle;
   };
 
+  // Does what it says
   const getName = (gender: string): string => {
 
     let nameIndex: number;
     if (gender === 'f') {
-      nameIndex = getRandomNumber(len.femaleNames);
+      nameIndex = getRandomNumber(femaleNames.length);
       return femaleNames[nameIndex];
     }
+
     if (gender === 'm') {
-      nameIndex = getRandomNumber(len.maleNames);
+      nameIndex = getRandomNumber(maleNames.length);
       return maleNames[nameIndex];
     }
-    nameIndex = getRandomNumber(len.neutralNames);
+
+    // gender === 'n'
+    nameIndex = getRandomNumber(neutralNames.length);
     return neutralNames[nameIndex];
   };
 
+  // They're not all surnames, don't @ me
   const getSurname = (gender: string): string => {
+
     let index: number;
+    let diceRoll: number;
+
+    // Dudes only
     if (gender === 'm') {
-      let chance: number = getRandomNumber(5);
-      if (chance < 1) {
+
+      // Not many to choose from, so we keep odds low
+      diceRoll = getRandomNumber(5);
+      if (diceRoll < 1) {
         index = getRandomNumber(maleSuffixes.length);
         return maleSuffixes[index];
       }
     }
 
-    let diceRoll: number = getRandomNumber(2);
+    // Anyone can have these
+    diceRoll = getRandomNumber(2);
     if (diceRoll === 0) {
-      index = getRandomNumber(len.surnames);
+      index = getRandomNumber(surnames.length);
       return `of ${surnames[index]}`;
     }
-    index = getRandomNumber(len.neutralSuffixes);
+
+    index = getRandomNumber(neutralSuffixes.length);
     return `the ${neutralSuffixes[index]}`;
   };
 
+  // Choose likes & dislikes
   const getActivities = (arr: string[], n: number): string[] => {
     const len = arr.length;
 
-    // Get likes & dislikes, check for duplicates
+    // Check for duplicates
     let activitySet = new Set<string>();
     while (activitySet.size < n) {
       let i: number = getRandomNumber(len);
       activitySet.add(arr[i]);
     }
+
     return [...activitySet];
   }
 
+  // Set all the mf state
   const handleClick = (): void => {
+
+    // This is to create a fade-in effect for the text
     setTimeout(() => {
       setFade(true);
-    }, 0);
-    // We add one so age isn't 0
-    setAge(getRandomNumber(25) + 1);
 
-    const gender: string = getGender();
-  
-    // Get names and titles
-    setTitle(getTitle(gender));
+      // We add one so age isn't 0
+      setAge(getRandomNumber(25) + 1);
 
-    const newName: string = getName(gender);
+      // Just so names & titles make sense
+      const gender: string = getGender();
+    
+      // Get names & titles & surnames or whatever
+      setTitle(getTitle(gender));
+      const newName: string = getName(gender);
+      setName(newName);
+      setSurname(getSurname(gender));
 
-    setName(newName);
-    setSurname(getSurname(gender));
 
+      // Select varying numbers of likes & dislikes
+      const diceRoll: number = getRandomNumber(2);
+      const newLikes: string[] = getActivities(likesArr, diceRoll === 0 ? VALUE1 : VALUE2);
+      setLikes(newLikes);
+      const newDislikes: string[] = getActivities(dislikesArr, diceRoll === 1 ? VALUE1 : VALUE2);
+      setDislikes(newDislikes);
 
-    // Select varying numbers of likes & dislikes
-    const diceRoll: number = getRandomNumber(2);
-    const newLikes: string[] = getActivities(likesArr, diceRoll === 0 ? VALUE1 : VALUE2);
-    setLikes(newLikes);
-    const newDislikes: string[] = getActivities(dislikesArr, diceRoll === 1 ? VALUE1 : VALUE2);
-    setDislikes(newDislikes);
+      // Choose verbs to describe likes & dislikes
+      const [newLikePhrase, newDislikePhrase]: number[] = [likeVerbs, dislikeVerbs].map(arr => getRandomNumber(arr.length));
+      setLikePhrase(likeVerbs[newLikePhrase]);
+      setDislikePhrase(dislikeVerbs[newDislikePhrase]);
 
-    // Choose verbs to describe likes & dislikes
-    const newLikePhrase: number = getRandomNumber(len.likeVerbs);
-    setLikePhrase(likeVerbs[newLikePhrase]);
-    const newDislikePhrase: number = getRandomNumber(len.dislikeVerbs);
-    setDislikePhrase(dislikeVerbs[newDislikePhrase]);
+      // Choose intro & outro
+      const [newIntro, newOutro]: number[] = [intros, outros].map(arr => getRandomNumber(arr.length));
+      setIntro(intros[newIntro]);
+      setOutro(outros[newOutro]);
 
-    // Choose intro & outro
-    const newIntro: number = getRandomNumber(len.intros);
-    setIntro(intros[newIntro]);
-    const newOutro: number = getRandomNumber(len.outros);
-    setOutro(outros[newOutro]);
-    setBreed('Domestic Semifloof');
+      setBreed('Domestic Semifloof');
+    }, 250);
   }
 
+  // So we can map our shiz to each fact component
   const fastFacts: string[][] = [
     [ 'name', `${title} ${name} ${surname}` ],
     [ 'age', age.toString() ],
     [ 'breed', breed ]
   ];
+
+  // Form coherent sentences for our component
+  const formattedLikes: string = formatActivities(likes, 'and');
+  const formattedDislikes: string = formatActivities(dislikes, 'or');
 
   return (
     <main className="main">
