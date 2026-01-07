68d34505aea35764dc5b88dcce4398f99feef66a
acopperlily <april.copley@protonmail.com>
2026-01-07 18:15:04 -0500

    Create form element

--- src/components/MainLogic.tsx
+++ src/components/MainLogic.tsx
@@ -35,10 +35,10 @@ const MainLogic = () => {
   const [breeds, setBreeds] = useState<BreedsObject>({rand: 'Random Breed'});
   const [imageURL, setImageURL] = useState<string>('');
   const [fade, setFade] = useState<boolean>(true);
-  const [triggerFetch, setTriggerFetch] = useState<boolean>(false);
   const [name, setName] = useState<string>('');
   const [age, setAge] = useState<number>(14);
   const [breed, setBreed] = useState<string>('rand');
+  const [selectedBreed, setSelectedBreed] = useState<string>('rand');
   const [title, setTitle] = useState<string>('');
   const [surname, setSurname] = useState<string>('');
   const [intro, setIntro] = useState<string>('');
@@ -50,6 +50,8 @@ const MainLogic = () => {
 
   // This state is used to keep the breed in state separate when "Random Breed" is selected in the dropdown...for reasons
   const [isRandom, setIsRandom] = useState<boolean>(true);
+
+  console.log('render');
 
   // Random & arbitrary af, but I do what I want
   const getGender = (): string => {
@@ -151,31 +153,120 @@ const MainLogic = () => {
     return [...activitySet];
   };
 
-  // Set all the mf state
-  const handleClick = (e: any): void => {
-    e.stopPropagation();
+// Set all the mf state
 
-    // Make sure we're not clicking on dropdown menu
-    if (e.target.id !== 'breeds') {
-      // Keep "Random Breed" selected in dropdown if already selected
-      setBreed(isRandom ? 'rand' : breed);
-      setTriggerFetch(!triggerFetch);
-    }
+  const getCat = async () => {
+    console.log('fetch called');
+    console.log('current breed in function:', breed);
+    setBreed(selectedBreed);
+    setIsLoading(true);
+    setFade(false);
+
+    // This cancels erroneous requests
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    try {
+      let URL = DOMAIN;
+      let res;
+
+      if (selectedBreed === 'rand') {
+        // Get either a random cat or one with a listed breed
+        let diceRoll = getRandomNumber(2);
+        if (diceRoll === 0) {
+          URL += `api_key=${API_KEY}&has_breeds=true`;
+        }
+        res = await axios.get(URL, { signal });
+        console.log('res:', res);
+
+        let newBreed;
+        if (diceRoll === 0) {
+          newBreed = res.data[0].breeds[0].id;
+          setBreed(newBreed);
+        }
+        setIsRandom(true);
+      } else {
+        res = await axios.get(`${URL}breed_ids=${selectedBreed}`, { signal });
+      }
+
+      setImageURL(res.data[0].url);
+      setError(false);
+
+    } catch (err) {
+      console.error('Error:', err);
+      setError(true);
+    } finally {
+
+      setTimeout(() => {
+      // All dynamic text is generated regardless of error
+      // We add one so age isn't 0
+      let age = getRandomNumber(MAX_AGE) + 1;
+
+      // Increase odds of choosing younger ages
+      if (age > MAX_AGE / 2) {
+        let diceRoll = getRandomNumber(3);
+        if (diceRoll > 0) {
+        age = Math.round(age / 2);
+        }
+      }
+      setAge(age);
+
+      // Just so names & titles make sense
+      const gender: string = getGender();
+
+      // Get names & titles & surnames or whatever
+      setTitle(getTitle(gender));
+      const newName: string = getName(gender);
+      setName(newName);
+      setSurname(getSurname(gender));
+
+      // Select varying numbers of likes & dislikes
+      const diceRoll: number = getRandomNumber(2);
+      const newLikes: string[] = getActivities(likesArr, diceRoll === 0 ? VALUE1 : VALUE2);
+      setLikes(newLikes);
+      const newDislikes: string[] = getActivities(dislikesArr, diceRoll === 1 ? VALUE1 : VALUE2);
+      setDislikes(newDislikes);
+
+      // Choose verbs to describe likes & dislikes
+      const [newLikePhrase, newDislikePhrase]: number[] = [likeVerbs,   dislikeVerbs].map(arr => getRandomNumber(arr.length));
+      setLikePhrase(likeVerbs[newLikePhrase]);
+      setDislikePhrase(dislikeVerbs[newDislikePhrase]);
+
+      // Choose intro & outro
+      const [newIntro, newOutro]: number[] = [intros, outros].map(arr =>  getRandomNumber(arr.length));
+
+      setIntro(intros[newIntro]);
+      setOutro(outros[newOutro]);
+      setIsLoading(false);
+      setFade(true);
+    }, 800);
+  }
+
+  return () => {
+    controller.abort();
+  }
+};
+
+  const handleClick = (e: any): void => {
+    e.preventDefault();
+    // setBreed(selectedBreed)
+    // setIsRandom(selectedBreed === 'rand');
+    console.log('handle click breed:', e.target);
+    getCat();
   };
 
   const handleChange = (e: any): void => {
+    e.preventDefault();
     e.stopPropagation();
+
     let newBreed = e.target.value;
-    if (newBreed === 'Random Breed') {
-      setIsRandom(true);
-    } else {
-      setIsRandom(false);
-    }
-    setBreed(newBreed);
-    setTriggerFetch(!triggerFetch);
+    setIsRandom(newBreed === 'rand');
+    console.log('handle change breed:', newBreed);
+    setSelectedBreed(newBreed);
   };
 
   useEffect(() => {
+    console.log('get breeds');
     const getBreeds = async () => {
       let breedsList = { rand: 'Random Breed' };
       try {
@@ -195,103 +286,8 @@ const MainLogic = () => {
     };
 
     getBreeds();
-  }, []);
-
-  useEffect(() => {
-    setIsLoading(true);
-    setFade(false);
-
-    // This cancels erroneous requests
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    const getCat = async () => {
-      try {
-        let URL = DOMAIN;
-        let res;
-
-        if (breed === 'rand') {
-          // Get either a random cat or one with a listed breed
-          let diceRoll = getRandomNumber(2);
-          if (diceRoll === 0) {
-            URL += `api_key=${API_KEY}&has_breeds=true`;
-          }
-          res = await axios.get(URL, { signal });
-          // blobRes = await axios.get(res.data[0].url, { responseType: 'blob', signal });
-          console.log('res:', res);
-          // console.log('blob:', blobRes);
-
-          let newBreed;
-          if (diceRoll === 0) {
-            newBreed = res.data[0].breeds[0].id;
-            setBreed(newBreed);
-          }
-          setIsRandom(true);
-        } else {
-          res = await axios.get(`${URL}breed_ids=${breed}`, { signal });
-          // blobRes = await axios.get(res.data[0].url, { signal, responseType: 'blob' });
-        }
-        // blobRes = await axios.get(res.data[0].url, {responseType: 'blob'});
-        // console.log('blob:', blobRes);
-        setImageURL(res.data[0].url);
-        setError(false);
-
-      } catch (err) {
-        console.error('Error:', err);
-        setError(true);
-      } finally {
-
-        setTimeout(() => {
-          // All dynamic text is generated regardless of error
-          // We add one so age isn't 0
-          let age = getRandomNumber(MAX_AGE) + 1;
-
-          // Increase odds of choosing younger ages
-          if (age > MAX_AGE / 2) {
-            let diceRoll = getRandomNumber(3);
-            if (diceRoll > 0) {
-            age = Math.round(age / 2);
-            }
-          }
-          setAge(age);
-
-          // Just so names & titles make sense
-          const gender: string = getGender();
-    
-          // Get names & titles & surnames or whatever
-          setTitle(getTitle(gender));
-          const newName: string = getName(gender);
-          setName(newName);
-          setSurname(getSurname(gender));
-
-          // Select varying numbers of likes & dislikes
-          const diceRoll: number = getRandomNumber(2);
-          const newLikes: string[] = getActivities(likesArr, diceRoll === 0 ? VALUE1 : VALUE2);
-          setLikes(newLikes);
-          const newDislikes: string[] = getActivities(dislikesArr, diceRoll === 1 ? VALUE1 : VALUE2);
-          setDislikes(newDislikes);
-
-          // Choose verbs to describe likes & dislikes
-          const [newLikePhrase, newDislikePhrase]: number[] = [likeVerbs, dislikeVerbs].map(arr => getRandomNumber(arr.length));
-          setLikePhrase(likeVerbs[newLikePhrase]);
-          setDislikePhrase(dislikeVerbs[newDislikePhrase]);
-
-          // Choose intro & outro
-          const [newIntro, newOutro]: number[] = [intros, outros].map(arr => getRandomNumber(arr.length));
-          setIntro(intros[newIntro]);
-          setOutro(outros[newOutro]);
-          setIsLoading(false);
-          setFade(true);
-        }, 800);
-
-      }
-    }
-
     getCat();
-    return () => {
-      controller.abort();
-    }
-  }, [triggerFetch]);
+  }, []);
 
   // So we can map our shiz to each fact component
   const fastFacts: string[][] = [
@@ -339,30 +335,44 @@ const MainLogic = () => {
             outro={outro}
           />
 
-          <div 
-            className="dropdown" 
-            id="dropdown__button"
-            onClick={e => handleClick(e)}
+          <form 
+            action=""
+            className="form"
+            onSubmit={e => handleClick(e)}
           >
-            <label 
-              className="dropdown__label" 
-              id="dropdown__label"
-              htmlFor="breeds"
+            <div className="form__group">
+              <label 
+                htmlFor="breeds"
+                id="dropdown__label"
+                className="form__label"
+              >
+                Choose Breed
+              </label>
+
+              <select
+                name="breeds"
+                id="breeds"
+                className="form__select"
+                onChange={e => handleChange(e)}
+                value={isRandom ? 'rand' : selectedBreed}
+                // onChange={e => setBreed(e.target.value)}
+              >
+                {Object.entries(breeds).map(([id, name]) => (
+                  <option key={id} value={id} className="form__option">{name}</option>
+                ))}
+              </select>
+            </div>
+
+            <button
+              type="submit"
+              // onClick={e => handleClick(e)}
+              className="button"
+              id="button"
             >
               Catalyze
-            </label>
-            <select
-              name="breeds"
-              id="breeds"
-              className='dropdown__select'
-              onChange={e => handleChange(e)}
-              value={isRandom ? 'rand' : breed}
-            >
-              {Object.entries(breeds).map(([id, name]) => (
-                <option key={id} value={id}>{name}</option>
-              ))}
-            </select>
-          </div>
+            </button>
+
+          </form>
         </section>
 
       </div>
